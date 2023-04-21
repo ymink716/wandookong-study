eddf35ecb0502f573c5b51da401a6211a30d3958
ymink716 <ymink716@gmail.com>
2023-04-21 13:43:57 +0900

    feat: 글로벌 예외 필터 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,19 @@ import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/logger.middleware';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './http-exception.filter';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer): any {
