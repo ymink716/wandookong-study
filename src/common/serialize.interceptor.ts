1c6e5c61b09f08c9a33439de1f2ce0a6c7f0041b
ymink716 <ymink716@gmail.com>
2023-05-05 18:30:51 +0900

    feat: dto에 유효성 검사 데코레이터 추가

--- src/common/serialize.interceptor.ts
+++ src/common/serialize.interceptor.ts
@@ -10,7 +10,7 @@ export class SerializeInterceptor implements NestInterceptor {
     return handler.handle().pipe(
       map((data) => {
         return plainToInstance(this.dto, data, {
-          excludeExtraneousValues: true,
+          excludeExtraneousValues: true, // response로 내보내지 않을 불필요한 값 제거
         });
       }),
     );
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail()
   readonly email: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(10)
   readonly nickname: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
   readonly password: string;
 }
